610f49ddabad5fa9056f20b795f2c7b55a9a4dea
Glitch (sapphire-road-cloudberry) <none>
2024-03-10 04:56:50 +0000

    💐👫 Checkpoint ./server.js:65642011/3040

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require('express-session');
+const passport = require('passport')
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
 
-app.route('/').get((req, res) => {
-res.render('index')
+app.route("/").get((req, res) => {
+  res.render("index", { title: "hello", message: "Please log in" });
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
