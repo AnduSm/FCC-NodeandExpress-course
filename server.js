e39ff5ac40468e21ea6f92e7c9e214273eaee978
Glitch (sapphire-road-cloudberry) <none>
2024-03-11 06:04:00 +0000

    👡👾 Checkpoint ./server.js:65642011/1976

--- server.js
+++ server.js
@@ -35,6 +35,32 @@ passport.deserializeUser((id, done) => {
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+  passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
+  }).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+
 app.route("/").get((req, res) => {
   res.render("index", { title: "hello", message: "Please log in" });
 });
