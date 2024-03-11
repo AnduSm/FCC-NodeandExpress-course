4150dca0ca9bdce550d8a5435a4278298cf9cd2f
Glitch (sapphire-road-cloudberry) <none>
2024-03-11 06:49:46 +0000

    🖼👋 Checkpoint ./server.js:65642011/3109 ./sample.env:65642011/118

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
-"use strict";
-require("dotenv").config();
-const express = require("express");
-const myDB = require("./connection");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
-const session = require("express-session");
-const passport = require("passport");
-const { ObjectID } = require("mongodb");
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "pug");
-app.set("views", "./views/pug");
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-myDB(async (client) => {
-  const myDataBase = await client.db("database").collection("users");
-
-  // Be sure to change the title
-  app.route("/").get((req, res) => {
-    // Change the response to render the Pug template
-    res.render("index", {
-      title: "Connected to Database",
-      message: "Please login",
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in'
     });
   });
-  
+
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
@@ -50,16 +47,13 @@ myDB(async (client) => {
     });
   });
   
-}).catch((e) => {
-  app.route("/").get((req, res) => {
-    res.render("index", { title: e, message: "Unable to connect to database" });
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
-app.listen();
-
-
-
+  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
