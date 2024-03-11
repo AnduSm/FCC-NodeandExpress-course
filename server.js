be39f4a233a24bb71770bb089689591d62927c8f
Glitch (sapphire-road-cloudberry) <none>
2024-03-11 07:23:59 +0000

    🗼🎭 Checkpoint ./server.js:65642011/1105

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const { ObjectID } = require('mongodb');
 
 const app = express();
@@ -27,13 +28,24 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+passport.use(new LocalStrategy((username, password, done) => {
+  myDataBase.findOne({ username: username }, (err, user) => {
+    console.log(`User ${username} attempted to log in.`);
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    if (password !== user.password) return done(null, false);
+    return done(null, user);
+  });
+}));
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
   app.route('/').get((req, res) => {
     res.render('index', {
       title: 'Connected to Database',
-      message: 'Please log in'
+      message: 'Please log in',
+      showLogin: true
     });
   });
 
@@ -52,6 +64,11 @@ myDB(async client => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
+
+app.route('/login').post((req, res) => {
+  passport.authenticate('local', { failureRedirect: '/' })
+  res.render('/profi')
+})
   
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
