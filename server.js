87d41fbb3cc922c601afb4d8638d68fdf0faa5c3
Glitch (sapphire-road-cloudberry) <none>
2024-03-11 08:11:20 +0000

    🚠👙 Checkpoint ./server.js:65642011/625 ./views/pug/profile.pug:65642011/68

--- server.js
+++ server.js
@@ -44,23 +44,31 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
   
-  app
- .route('/profile')
- .get(ensureAuthenticated, (req,res) => {
-    res.render('profile');
- });
   
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
   res.redirect('/profile');
-})
- app.route('/profile').get((req,res) => {
-    res.render('profile');
-  })
-
+});
+  
+  app.route('/profile').get(ensureAuthenticated, (req,res) => {
+    res.render('profile', { username: req.user.username });
+ });
+  
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+  
+  app.use((req, res, next) => {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+  
   passport.use(new LocalStrategy((username, password, done) => {
   myDataBase.findOne({ username: username }, (err, user) => {
     console.log(`User ${username} attempted to log in.`);
