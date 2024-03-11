c7189ddead4eb835636a41ec541c06920ba2a7e7
Glitch (sapphire-road-cloudberry) <none>
2024-03-11 06:15:58 +0000

    🍇🐯 Checkpoint ./server.js:65642011/4035 ./sample.env:65642011/431

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-const session = require('express-session');
-const passport = require('passport')
-const { ObjectID } = require('mongodb');
+const session = require("express-session");
+const passport = require("passport");
+const { ObjectID } = require("mongodb");
 
 const app = express();
 
@@ -13,53 +13,46 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  });
-//});
 
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 
-myDB(async client => {
-  const myDataBase = await client.db('database').collection('users');
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
 
   // Be sure to change the title
-  app.route('/').get((req, res) => {
+  app.route("/").get((req, res) => {
     // Change the response to render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login'
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
     });
   });
   passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to connect to database" });
   });
 });
-  }).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
-  });
-});
+app.listen();
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "hello", message: "Please log in" });
