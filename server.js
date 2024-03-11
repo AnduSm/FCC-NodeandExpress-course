5a2ac7a4485dc08e6ef361a357bd81df2c479a33
Glitch (sapphire-road-cloudberry) <none>
2024-03-11 08:46:09 +0000

    🙋🍦 Checkpoint ./server.js:65642011/636 ./auth.js:65642011/48 ./routes.js:65642011/48

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
+
 
 const app = express();
 
@@ -33,6 +37,9 @@ app.use(express.urlencoded({ extended: true }));
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+  
   function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -50,6 +57,7 @@ myDB(async client => {
   });
   
   app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
     myDatabase.findOne({ username: req.body.username }, (err, user) => {
       if(err){
         next(err);
@@ -58,7 +66,7 @@ myDB(async client => {
       } else {
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password
+          password: hash
         },
           (err, doc) =>{
           if (err) {
@@ -102,7 +110,9 @@ myDB(async client => {
     console.log(`User ${username} attempted to log in.`);
     if (err) return done(err);
     if (!user) return done(null, false);
-    if (password !== user.password) return done(null, false);
+   if (!bcrypt.compareSync(password, user.password)) { 
+  return done(null, false);
+}
     return done(null, user);
   });
 }));
