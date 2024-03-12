c7516e2e4d1ab91b8144029749e8892b4dabb80f
Glitch (sapphire-road-cloudberry) <none>
2024-03-12 05:31:34 +0000

    🚤🌎 Checkpoint ./server.js:65642011/355 ./public/client.js:65642011/594 ./routes.js:65642011/1

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const routes = require('./routes.js');
 const auth = require('./auth.js');
 
 
+
 const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -39,6 +43,11 @@ myDB(async client => {
   
   routes(app, myDataBase);
   auth(app, myDataBase);
+  
+  io.on('connection', socket => {
+  console.log('A user has connected');
+});
+  
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
@@ -47,6 +56,6 @@ myDB(async client => {
 
   
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
